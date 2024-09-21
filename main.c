b4ad2e400efaf4d8b56ba502854830a8ef586654
MMansurii <mansurimohamad92@gmail.com>
2024-09-21 10:22:09 +0200

    Update main.c

--- main.c
+++ main.c
@@ -307,6 +307,36 @@ int	is_digit(char *args)
 	return (1);
 }
 
+int	ft_isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+int	ft_atoi(const char *str)
+{
+	int	nb;
+	int	flag;
+	int	i;
+
+	nb = 0;
+	i = 0;
+	flag = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			flag *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nb = (nb * 10) + (str[i] - 48);
+		i++;
+	}
+	return (nb * flag);
+}
+
 int	is_valid_integer(char **args)
 {
 	int		i;
